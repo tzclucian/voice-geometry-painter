3e8a4caef42545d624e9ad40d3f53f39ea6ccfad
Marian-Pinzariu <mpinzariu@gmail.com>
2015-05-12 03:28:35 +0300

    Added Speech related separate scripts and images

---
+++ vgp-web/js/speech/Speech.js
@@ -0,0 +1,256 @@
+/**
+ * Created by Marian on 5/11/2015.
+ */
+
+var Speech = function() {
+
+    this.commands = [ /*0*/'', /*1*/'draw', /*2*/'rectangle', /*3*/'triangle', /*4*/'circle',
+                      /*5*/'A', /*6*/'B'];
+
+    this.stateMachine = [[1], [2, 3, 4], [5, 6], [5, 6], [5, 6]];
+
+    this.state = 0;
+
+    this.langs =
+        [
+            ['Afrikaans',
+                ['af-ZA']
+            ],
+            ['Bahasa Indonesia',
+                ['id-ID']
+            ],
+            ['Bahasa Melayu',
+                ['ms-MY']
+            ],
+            ['Català',
+                ['ca-ES']
+            ],
+            ['?eština',
+                ['cs-CZ']
+            ],
+            ['Deutsch',
+                ['de-DE']
+            ],
+            ['English',
+                ['en-AU', 'Australia'],
+                ['en-CA', 'Canada'],
+                ['en-IN', 'India'],
+                ['en-NZ', 'New Zealand'],
+                ['en-ZA', 'South Africa'],
+                ['en-GB', 'United Kingdom'],
+                ['en-US', 'United States']
+            ],
+            ['Español',
+                ['es-AR', 'Argentina'],
+                ['es-BO', 'Bolivia'],
+                ['es-CL', 'Chile'],
+                ['es-CO', 'Colombia'],
+                ['es-CR', 'Costa Rica'],
+                ['es-EC', 'Ecuador'],
+                ['es-SV', 'El Salvador'],
+                ['es-ES', 'España'],
+                ['es-US', 'Estados Unidos'],
+                ['es-GT', 'Guatemala'],
+                ['es-HN', 'Honduras'],
+                ['es-MX', 'México'],
+                ['es-NI', 'Nicaragua'],
+                ['es-PA', 'Panamá'],
+                ['es-PY', 'Paraguay'],
+                ['es-PE', 'Perú'],
+                ['es-PR', 'Puerto Rico'],
+                ['es-DO', 'República Dominicana'],
+                ['es-UY', 'Uruguay'],
+                ['es-VE', 'Venezuela']
+            ],
+            ['Euskara',
+                ['eu-ES']
+            ],
+            ['Français',
+                ['fr-FR']
+            ],
+            ['Galego',
+                ['gl-ES']
+            ],
+            ['Hrvatski',
+                ['hr_HR']
+            ],
+            ['IsiZulu',
+                ['zu-ZA']
+            ],
+            ['Íslenska',
+                ['is-IS']
+            ],
+            ['Italiano',
+                ['it-IT', 'Italia'],
+                ['it-CH', 'Svizzera']
+            ],
+            ['Magyar',
+                ['hu-HU']
+            ],
+            ['Nederlands',
+                ['nl-NL']
+            ],
+            ['Norsk bokmål',
+                ['nb-NO']
+            ],
+            ['Polski',
+                ['pl-PL']
+            ],
+            ['Português',
+                ['pt-BR', 'Brasil'],
+                ['pt-PT', 'Portugal']
+            ],
+            ['Român?',
+                ['ro-RO']
+            ],
+            ['Sloven?ina',
+                ['sk-SK']
+            ],
+            ['Suomi',
+                ['fi-FI']
+            ],
+            ['Svenska',
+                ['sv-SE']
+            ],
+            ['Türkçe',
+                ['tr-TR']
+            ],
+            ['?????????',
+                ['bg-BG']
+            ],
+            ['P??????',
+                ['ru-RU']
+            ],
+            ['??????',
+                ['sr-RS']
+            ],
+            ['???',
+                ['ko-KR']
+            ],
+            ['??',
+                ['cmn-Hans-CN', '??? (????)'],
+                ['cmn-Hans-HK', '??? (??)'],
+                ['cmn-Hant-TW', '?? (??)'],
+                ['yue-Hant-HK', '?? (??)']
+            ],
+            ['???',
+                ['ja-JP']
+            ],
+            ['Lingua lat?na',
+                ['la']
+            ]
+        ];
+    this.selectedLanguageIndex = 6;
+    this.selectedDialectIndex = 7;
+    this.finalTranscript = '';
+    this.recognizing = false;
+    this.ignoreOnend = false;
+    this.startTimestamp = null;
+    this.recognition = null;
+    this.interimResult = null;
+    this.finalResult = null;
+    this.onStart = null;
+    this.onEnd = null;
+    this.onError = null;
+    this.onResult = null;
+};
+
+Speech.prototype.init = function(){
+    if (!('webkitSpeechRecognition' in window)) {
+        alert('Web Speech API is not supported by this browser. ' +
+              'Upgrade to Chrome version 25 or later.');
+    } else {
+        this.recognition = new webkitSpeechRecognition();
+        this.recognition.continuous = true;
+        this.recognition.interimResults = true;
+    }
+};
+
+Speech.prototype.start = function() {
+    this.finalTranscript = '';
+    this.recognition.lang = this.langs[this.selectedLanguageIndex][this.selectedDialectIndex];
+    this.recognition.start();
+    this.ignoreOnend = false;
+    this.interimResult = '';
+    this.finalResult = '';
+};
+
+Speech.prototype.onStartCallback = function() {
+    this.recognizing = true;
+    //if ( window.console && window.console.log ) {
+    //    window.console.log('Start...');
+    //}
+    this.onStart();
+};
+
+Speech.prototype.registerOnStart = function(onStartFunction) {
+    this.onStart = onStartFunction;
+    var obj = this;
+    this.recognition.onstart = function(){obj.onStartCallback(event)};
+};
+
+Speech.prototype.onErrorCallback = function(event) {
+    //if ( window.console && window.console.log ) {
+    //    window.console.log('Error: ' + event);
+    //}
+    this.onError(event);
+    ignore_onend = true;
+};
+
+Speech.prototype.registerOnError = function(onErrorFunction) {
+    this.onError = onErrorFunction;
+    var obj = this;
+    this.recognition.onerror = function(event){obj.onErrorCallback(event)};
+};
+
+Speech.prototype.onEndCallback = function() {
+    this.recognizing = false;
+    //if ( window.console && window.console.log ) {
+    //    window.console.log('End...');
+    //}
+    if (this.ignore_onend) {
+        return;
+    }
+    this.onEnd();
+};
+
+Speech.prototype.registerOnEnd = function(onEndFunction) {
+    this.onEnd = onEndFunction;
+    var obj = this;
+    this.recognition.onend = function(){obj.onEndCallback()};
+};
+
+Speech.prototype.onResultCallback = function(event) {
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+        //if ( window.console && window.console.log ) {
+        //    window.console.log('Result[' + event.results[i].isFinal + ']: ' + event.results[i][0].transcript);
+        //}
+        //var result = event.results[i][0].transcript;
+        //var tokens = result.split(' ');
+        //for (var j = 0; j < tokens.length; ++j) {
+        //    if (this.isCommand(tokens[j])) {
+        //        this.onResult(tokens[j]);
+        //    }
+        //}
+        this.onResult(event.results[i][0].transcript);
+    }
+};
+
+Speech.prototype.registerOnResult = function(onResultFunction) {
+    this.onResult = onResultFunction;
+    var obj = this;
+    this.recognition.onresult = function(event){obj.onResultCallback(event)};
+};
+
+Speech.prototype.isCommand = function(token) {
+    for (var i = 0; i < this.stateMachine[this.state].length; ++i) {
+        if (this.commands[this.stateMachine[this.state][i]] == token) {
+            this.state = this.stateMachine[this.state][i];
+            //if ( window.console && window.console.log ) {
+            //    window.console.log('Command ' + token + ' matched -> state:' + this.state);
+            //}
+            return true;
+        }
+    }
+    return false;
+};
---
+++ vgp-web/js/speech_logic.js
@@ -0,0 +1,81 @@
+/**
+ * Created by Marian on 5/12/2015.
+ */
+
+var SpeechApplication = function() {
+    this.speech = new Speech();
+    this.micNormalSrc = '';
+    this.micAnimatedSrc = '';
+    this.micStoppedSrc = '';
+    this.micImg = null;
+    this.outputBox = null;
+};
+
+SpeechApplication.prototype.init = function(){
+    this.speech.init();
+    var obj = this;
+    this.speech.registerOnStart(function(){obj.onStart()});
+    this.speech.registerOnError(function(event){obj.onError(event)});
+    this.speech.registerOnEnd(function(){obj.onEnd()});
+    this.speech.registerOnResult(function(event){obj.onResult(event)});
+    this.speech.start();
+};
+
+SpeechApplication.prototype.setMicImgId = function(id) {
+    this.micImg = id;
+};
+
+SpeechApplication.prototype.ClearMicImgId = function() {
+    this.micImg = null;
+};
+
+SpeechApplication.prototype.setOutputBoxId = function(id) {
+    this.outputBox = id;
+};
+
+SpeechApplication.prototype.ClearOutputBoxId = function() {
+    this.outputBox = null;
+};
+
+SpeechApplication.prototype.setMicImageSources = function(normal, animated, stopped) {
+    this.micNormalSrc = normal;
+    this.micAnimatedSrc = animated;
+    this.micStoppedSrc = stopped;
+};
+
+SpeechApplication.prototype.onStart = function() {
+    if (this.micImg != null) {
+        this.micImg.src = this.micAnimatedSrc;
+    }
+};
+
+SpeechApplication.prototype.onEnd = function() {
+    if (this.micImg != null) {
+        this.micImg.src = this.micNormalSrc;
+    }
+};
+
+SpeechApplication.prototype.onResult = function(phrase) {
+    if (this.outputBox != null) {
+        this.outputBox.value = phrase;
+    }
+};
+
+SpeechApplication.prototype.onError = function(event) {
+    if (event.error == 'no-speech') {
+        if (this.micImg != null) {
+            this.micImg.src = this.micStoppedSrc;
+        }
+        alert('No speech was detected. You may need to adjust your microphone settings.');
+    }
+    if (event.error == 'audio-capture') {
+        if (this.micImg != null) {
+            this.micImg.src = this.micStoppedSrc;
+        }
+        alert('No microphone was found. Ensure that a microphone is installed and that' +
+              'microphone settings are configured correctly.');
+    }
+    if (event.error == 'not-allowed') {
+        alert('Permission to use microphone is blocked or denied.');
+    }
+};
