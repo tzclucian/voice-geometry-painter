722faa3c8814e4cf9357c7c5b6a792bf70af6ed9
Lucian Tuca <tzclucian@gmail.com>
2015-05-20 22:47:19 +0300

    New design.

--- vgp-web/js/setup.js
+++ vgp-web/js/setup.js
@@ -4,14 +4,15 @@ function AppViewModel() {
 
 	this.commands = [];
 
-	this.config = {
-		vocalCommandHelpMessages: {
-			SetColor: SetColorCommand.prototype.HELP,
-			SetWidth: SetWidthCommand.prototype.HELP,
+    this.config = {
+        vocalCommandHelpMessages: {
+            SetLineColor: SetLineColorCommand.prototype.HELP,
+            SetFillColor: SetFillColorCommand.prototype.HELP,
+            SetWidth: SetWidthCommand.prototype.HELP,
 
-			ClearBoard: ClearBoardCommand.prototype.HELP,
-			DeleteSelected: DeleteSelectedCommand.prototype.HELP,
-			Deselect: DeselectCommand.prototype.HELP,
+            ClearBoard: ClearBoardCommand.prototype.HELP,
+            DeleteSelected: DeleteSelectedCommand.prototype.HELP,
+            Deselect: DeselectCommand.prototype.HELP,
 
 			Point: PointCommand.prototype.HELP,
 			Line: LineCommand.prototype.HELP,
@@ -28,25 +29,25 @@ ko.applyBindings(new AppViewModel());
 
 $(document).ready(function () {
 
-	$('.edit').editable(function (value, settings) {
-		return (value);
-	}, {
-		type: 'text',
-		cssclass: 'white-board-title-input',
-	});
+    $('.edit').editable(function (value, settings) {
+        return (value);
+    }, {
+        type: 'text',
+        cssclass: 'white-board-title-input',
+    });
 
-	$('#menuBtn').on('click', function () {
-		var element = $('.horizontal-expand');
-		var isMenuShown = element.css("display") === "block";
+    $('#menuBtn').on('click', function () {
+        var element = $('.horizontal-expand');
+        var isMenuShown = element.css("display") === "block";
 
-		if (isMenuShown) {
-			element.css("display", "none");
-			$('.menu-btn').removeClass('active');
-		} else {
-			element.css("display", "block");
-			$('.menu-btn').addClass('active');
-		}
-	});
+        if (isMenuShown) {
+            element.css("display", "none");
+            $('.menu-btn').removeClass('active');
+        } else {
+            element.css("display", "block");
+            $('.menu-btn').addClass('active');
+        }
+    });
 });
 
 var app = new DrawingApplication();
@@ -54,13 +55,15 @@ app.setupDrawingContext('jxgbox');
 app.activeDrawingContext = app.getContext('jxgbox');
 
 app.setCommandParser(new CommandParser());
+
 app.registerCommand(ClearBoardCommand);
 app.registerCommand(PointCommand);
 app.registerCommand(LineCommand);
 app.registerCommand(DeselectCommand);
 app.registerCommand(DeleteSelectedCommand);
 app.registerCommand(SetWidthCommand);
-app.registerCommand(SetColorCommand);
+app.registerCommand(SetLineColorCommand);
+app.registerCommand(SetFillColorCommand);
 
 var speech = new SpeechApplication();
 speech.setMicImgId(mic_img);
